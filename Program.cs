12100905cd7b3a5f70cedbeae4ca9da334889be0
Hochheim Christian <chochheim@pb-group.de>
2020-06-16 15:06:20 +0200

    Implementierung des SQLite-Datenbank-Zugriffs. Leider flache Implementierung, aber vielleicht kommt ja noch was dazu.

---
+++ Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections;
+using StockcheckDatabase;
+using System.Data;
+
+namespace StockcheckSQLiteDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLiteDatabaseAccess.DeleteTable("AAA");
+            DataTable newTable = SQLiteDatabaseAccess.CreateTable("AAA");
+            newTable.Rows.Add();
+            newTable.Rows.Add();
+            newTable.Rows[0]["Jahr"] = 2020;
+
+            newTable.Rows[1]["Jahr"] = 2009;
+
+            Console.WriteLine("Tabelle vorher:");
+            PrintTable(newTable);
+
+            AddRowInBetween(ref newTable);
+            AddRowInBetween(ref newTable);
+            AddRowInBetween(ref newTable);
+
+            Console.WriteLine("Tabelle sortiert:");
+            newTable.DefaultView.Sort = "Jahr Asc";
+            newTable = newTable.DefaultView.ToTable();
+            PrintTable(newTable);
+
+            Console.WriteLine("\nTabelle speichern.\n");
+
+            SQLiteDatabaseAccess.SaveTable(newTable);
+
+            Console.WriteLine("\nTabelle ausgeben.\n");
+
+            var tableList = SQLiteDatabaseAccess.GetTableNames();
+            PrintTableList(tableList);
+            Console.WriteLine("AAA");
+            PrintTable(SQLiteDatabaseAccess.LoadDataTable("AAA"));
+
+            Console.WriteLine("\nZeile löschen.\n");
+            DeleteRow(newTable, 2020);
+
+            Console.WriteLine("\nTabelle speichern.\n");
+
+            SQLiteDatabaseAccess.SaveTable(newTable);
+
+            Console.WriteLine("\nTabelle ausgeben.\n");
+            PrintTable(SQLiteDatabaseAccess.LoadDataTable("AAA"));
+
+            Console.WriteLine("\nTabelle wieder löschen.\n");
+
+            SQLiteDatabaseAccess.DeleteTable("AAA");
+            tableList = SQLiteDatabaseAccess.GetTableNames();
+            PrintTableList(tableList);
+        }
+
+        static void DeleteRow(DataTable table, int year)
+        {
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                DataRow dr = table.Rows[i];
+                if ((int)dr["Jahr"] == year)
+                    dr.Delete();
+            }
+            table.AcceptChanges();
+        }
+
+        // Bevorzugt wird eine "Lücke" befüllt. Gibt es keine "Lücke", wird am "Ende" angefügt.
+        static void AddRow(ref DataTable table)
+        {
+            if(!AddRowInBetween(ref table))
+            {
+                AddRowAfterLastYear(ref table);
+            }
+        }
+
+        // Fügt eine Zeile hinzu, wenn es eine "Lücke" in den Jahren gibt.
+        // Gibt es mehrere "Lücken", so wird die früheste Lücke (kleinst möglichstes Jahr) befüllt.
+        // return true: es gab eine Lücke und es wurde eine Zeile zugefügt
+        // return false: es gab keine Lücke und es wurde keine Zeile zugefügt
+        static bool AddRowInBetween(ref DataTable table)
+        {
+            table.DefaultView.Sort = "Jahr Asc";
+            table = table.DefaultView.ToTable();
+
+            int minYear = Convert.ToInt32(table.Compute("min([Jahr])", string.Empty));
+
+            // ermittle die erste Lücke
+            int yearBeforeGap = -1;
+            for (int i = 1; i < table.Rows.Count; i++)
+            {
+                int yearBefore = (int)table.Rows[i - 1]["Jahr"];
+                if ((int)table.Rows[i]["Jahr"] != yearBefore + 1)
+                {
+                    yearBeforeGap = (int)table.Rows[i-1]["Jahr"];
+                    break;
+                }
+            }
+
+            if (yearBeforeGap != -1)
+            {
+                DataRow row = table.NewRow();
+                row["Jahr"] = yearBeforeGap + 1;
+                table.Rows.Add(row);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        static void AddRowAfterLastYear(ref DataTable table)
+        {
+            int maxYear = Convert.ToInt32(table.Compute("max([Jahr])", string.Empty));
+
+            DataRow row = table.NewRow();
+            row["Jahr"] = maxYear + 1;
+            table.Rows.Add(row);
+        }
+
+        static void AddRowBeforeEarliestYear(ref DataTable table)
+        {
+            int minYear = Convert.ToInt32(table.Compute("min([Jahr])", string.Empty));
+
+            DataRow row = table.NewRow();
+            row["Jahr"] = minYear - 1;
+            table.Rows.Add(row);
+        }
+
+        static void PrintTableList(ArrayList tableList)
+        {
+            foreach (var table in tableList)
+            {
+                Console.WriteLine(table);
+            }
+        }
+
+        static void PrintTable(DataTable table)
+        {
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                Console.Write("Jahr: ");
+                Console.Write(table.Rows[i]["Jahr"]);
+                Console.Write(" Gewinn pro Aktie: ");
+                Console.Write(table.Rows[i]["Gewinn pro Aktie"]);
+                Console.Write(" Dividende pro Aktie: ");
+                Console.Write(table.Rows[i]["Dividende pro Aktie"]);
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ SQLiteDatabaseAccess.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Data;
+using System.Data.SQLite;
+using System;
+
+namespace StockcheckDatabase
+{
+    public static class SQLiteDatabaseAccess
+    {
+        public static DataTable LoadDataTable(string tableName)
+        {
+            SQLiteConnection sQLiteConnection = new SQLiteConnection("Data Source=" + "C:\\Users\\chochheim\\Documents\\CSharpProjects\\StockcheckSQLiteDatabase\\bin\\Debug\\netcoreapp3.1\\database\\Deutschland.db");
+
+            sQLiteConnection.Open();
+            DataTable table;
+            try
+            {
+                SQLiteDataAdapter sQLiteAdapter = new SQLiteDataAdapter("SELECT * FROM [" + tableName + "]", sQLiteConnection);
+                table = new DataTable();
+                sQLiteAdapter.Fill(table);
+                new SQLiteCommandBuilder(sQLiteAdapter);
+            }
+            catch (System.Exception)
+            {
+                sQLiteConnection.Close();
+                return null;
+            }
+            sQLiteConnection.Close();
+
+            table.TableName = tableName;
+            return ConvenienceTableFromDatabaseTable(table);
+        }
+
+        public static DataTable CreateTable(string tablename)
+        {
+            SQLiteConnection sQLiteConnection = new SQLiteConnection("Data Source=" + "C:\\Users\\chochheim\\Documents\\CSharpProjects\\StockcheckSQLiteDatabase\\bin\\Debug\\netcoreapp3.1\\database\\Deutschland.db");
+
+            sQLiteConnection.Open();
+            try
+            {
+                using (SQLiteCommand mCmd = new SQLiteCommand("CREATE TABLE IF NOT EXISTS ["
+                    + tablename + "] " +
+                    "('year' INTEGER," +
+                    " 'earning_per_share' FLOAT," +
+                    " 'div_per_share' FLOAT);",
+                    sQLiteConnection))
+                {
+                    int result=mCmd.ExecuteNonQuery();
+                }
+            }
+            catch(System.Exception)
+            {
+                 sQLiteConnection.Close();
+            }
+            sQLiteConnection.Close();
+
+            return LoadDataTable(tablename);
+        }
+
+        public static ArrayList GetTableNames()
+        {
+            SQLiteConnection sQLiteConnection = new SQLiteConnection("Data Source=" + "C:\\Users\\chochheim\\Documents\\CSharpProjects\\StockcheckSQLiteDatabase\\bin\\Debug\\netcoreapp3.1\\database\\Deutschland.db");
+            ArrayList itemsList = new ArrayList();
+
+            sQLiteConnection.Open();
+            using (DataTable mTables = sQLiteConnection.GetSchema("Tables"))
+            {
+                for (int i = 0; i < mTables.Rows.Count; i++)
+                {
+                    itemsList.Add(mTables.Rows[i].ItemArray[mTables.Columns.IndexOf("TABLE_NAME")].ToString());
+                }
+            }
+            sQLiteConnection.Close();
+
+            return itemsList;
+        }
+
+        public static bool SaveTable(DataTable table)
+        {
+            Execute(string.Format("DELETE FROM {0}", table.TableName));
+
+            SQLiteConnection sQLiteConnection = new SQLiteConnection("Data Source=" + "C:\\Users\\chochheim\\Documents\\CSharpProjects\\StockcheckSQLiteDatabase\\bin\\Debug\\netcoreapp3.1\\database\\Deutschland.db");
+
+            sQLiteConnection.Open();
+            try
+            {
+                var cmd = sQLiteConnection.CreateCommand();
+                cmd.CommandText = string.Format("SELECT * FROM [{0}]", table.TableName);
+                SQLiteDataAdapter _sQLiteAdapter = new SQLiteDataAdapter(cmd);
+                SQLiteCommandBuilder builder = new SQLiteCommandBuilder(_sQLiteAdapter);
+                builder.GetInsertCommand();
+
+                var ret = _sQLiteAdapter.Update(DatabaseTableFromConvenienceTable(table));
+                sQLiteConnection.Close();
+                return true;
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.Message);
+                sQLiteConnection.Close();
+                return false;
+            }
+        }
+
+        public static bool DeleteTable(string tablename)
+        {
+            SQLiteConnection sQLiteConnection = new SQLiteConnection("Data Source=" + "C:\\Users\\chochheim\\Documents\\CSharpProjects\\StockcheckSQLiteDatabase\\bin\\Debug\\netcoreapp3.1\\database\\Deutschland.db");
+
+            sQLiteConnection.Open();
+            try
+            {
+                string query = string.Format("DROP TABLE '{0}'", tablename);
+                using (SQLiteCommand mCmd = new SQLiteCommand(query, sQLiteConnection))
+                {
+                    int result = mCmd.ExecuteNonQuery();
+                    if (result == -1) return false;
+                }
+            }
+            catch (System.Exception)
+            {
+                sQLiteConnection.Close();
+                return false;
+            }
+            sQLiteConnection.Close();
+            return true;
+        }
+
+        private static DataTable ConvenienceTableFromDatabaseTable(DataTable databaseTable)
+        {
+            var convenienceTable = new DataTable();
+
+            convenienceTable.Columns.Add("Jahr", typeof(int));
+            convenienceTable.Columns.Add("Gewinn pro Aktie", typeof(float));
+            convenienceTable.Columns.Add("Dividende pro Aktie", typeof(float));
+
+            for (int i = 0; i < databaseTable.Rows.Count; i++)
+            {
+                convenienceTable.Rows.Add();
+                convenienceTable.Rows[i]["Jahr"] = databaseTable.Rows[i]["year"];
+                convenienceTable.Rows[i]["Gewinn pro Aktie"] = databaseTable.Rows[i]["earning_per_share"];
+                convenienceTable.Rows[i]["Dividende pro Aktie"] = databaseTable.Rows[i]["div_per_share"];
+            }
+
+            convenienceTable.TableName = databaseTable.TableName;
+            return convenienceTable;
+        }
+        private static DataTable DatabaseTableFromConvenienceTable(DataTable databaseTable)
+        {
+            var convenienceTable = new DataTable();
+
+            convenienceTable.Columns.Add("year", databaseTable.Columns["Jahr"].DataType);
+            convenienceTable.Columns.Add("earning_per_share", databaseTable.Columns["Gewinn pro Aktie"].DataType);
+            convenienceTable.Columns.Add("div_per_share", databaseTable.Columns["Dividende pro Aktie"].DataType);
+
+            for (int i = 0; i < databaseTable.Rows.Count; i++)
+            {
+                convenienceTable.Rows.Add();
+                convenienceTable.Rows[i]["year"] = databaseTable.Rows[i]["Jahr"];
+                convenienceTable.Rows[i]["earning_per_share"] = databaseTable.Rows[i]["Gewinn pro Aktie"];
+                convenienceTable.Rows[i]["div_per_share"] = databaseTable.Rows[i]["Dividende pro Aktie"];
+            }
+
+            return convenienceTable;
+        }
+
+        private static int Execute(string sql_statement)
+        {
+            SQLiteConnection con = new SQLiteConnection("Data Source=" + "C:\\Users\\chochheim\\Documents\\CSharpProjects\\StockcheckSQLiteDatabase\\bin\\Debug\\netcoreapp3.1\\database\\Deutschland.db");
+
+            con.Open();
+            var cmd = con.CreateCommand();
+            cmd.CommandText = sql_statement;
+            int row_updated;
+            try
+            {
+                row_updated = cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                con.Close();
+                return 0;
+            }
+            con.Close();
+            return row_updated;
+        }
+    }
+}
